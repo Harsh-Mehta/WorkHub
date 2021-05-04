ae57542d7ebd6d22437a3505f654db63f9991699
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-04 22:39:22 +0100

    Added code for initialising flask application along with database and plugins.

--- application/__init__.py
+++ application/__init__.py
@@ -1,2 +1,31 @@
-def create_app():
-    pass
+"""Initialize app."""
+
+from flask import Flask
+
+
+def init_app():
+    """Construct the core app object."""
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+
+    # Application Configuration
+    app.config.from_object('config.Config')
+
+    # Initialize Plugins
+    from .extensions import db, login_manager, migrate
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+
+    with app.app_context():
+        from . import routes
+        from . import auth
+        
+        # Register Blueprints
+        app.register_blueprint(auth.auth_bp)
+
+        # Create Database Models
+        db.create_all()
+        
+        return app
