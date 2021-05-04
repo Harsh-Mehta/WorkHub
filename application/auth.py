fdde55668fb4ce6dd306474b67201932d3e1936a
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-04 23:24:20 +0100

    Added handlers for checking if the user is logged in on page load.

--- application/auth.py
+++ application/auth.py
@@ -1,8 +1,9 @@
 """Routes for user authentication."""
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from .forms import LoginForm
 from .models import User
+from .extensions import login_manager
 from flask_login import current_user, login_user
 from flask.helpers import flash
 
@@ -40,3 +41,20 @@ def login():
 # def signup():
 #     # Signup logic goes here
 #     pass
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Check if user is logged-in on every page load."""
+    
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth_bp.login'))
+
