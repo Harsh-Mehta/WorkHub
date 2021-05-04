0c552bd5c4a92a557314173f6cdceb404b9f4e31
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-04 23:25:05 +0100

    Added route for user logout.

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,17 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask import current_app as app
+from flask_login import logout_user
 
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     return "This is homepage."
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """User log-out logic."""
+    logout_user()
+    return redirect(url_for('auth_bp.login'))
