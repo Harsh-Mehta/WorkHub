e3225f7bc7879cd860dd98233f8dc3cd4015c09c
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-05 14:42:10 +0100

    Associated home route with the root route.

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, redirect, url_for
 from flask import current_app as app
-from flask_login import logout_user
+from flask_login import logout_user, login_required
 
 
 
 @app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 def home():
     return "This is homepage."
 
