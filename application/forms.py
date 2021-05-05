e8814f78bbe2870a0addbf2fa6ef8606ebe6b541
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-05 14:36:56 +0100

    Added a SignupForm class and remember me checkbox.

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField
 from wtforms.validators import (
     DataRequired,
     Email,
@@ -7,6 +7,44 @@ from wtforms.validators import (
     Length,
     Optional
 )
+
+
+class SignupForm(FlaskForm):
+    """User Sign-up Form."""
+    
+    fname = StringField(
+        'First Name',
+        validators=[DataRequired()]
+    )
+    lname = StringField(
+        'Last Name',
+        validators=[DataRequired()]
+    )
+    email = StringField(
+        'Email',
+        validators=[
+            Length(min=6),
+            Email(message='Enter a valid email.'),
+            DataRequired()
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=6, message='Select a stronger password.')
+        ]
+    )
+    confirm = PasswordField(
+        'Confirm Your Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
+        ]
+    )
+    contact = StringField('Contact Number', validators=[DataRequired()])
+    role = RadioField('Role', validators=[DataRequired()], choices=["Job Seeker", "Recruiter", "Admin"])
+    submit = SubmitField('Register')
 
 
 class LoginForm(FlaskForm):
@@ -20,4 +58,5 @@ class LoginForm(FlaskForm):
         ]
     )
     password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
