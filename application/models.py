c30497e30a9c1e3f8563ba2746920e882ebd57f5
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-07 17:33:31 +0100

    Added relationship between database models - User and Role.

--- application/models.py
+++ application/models.py
@@ -4,11 +4,13 @@ from .extensions import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class Role(db.Model):
     __tablename__ = "roles"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
+    users = db.relationship('User', backref=db.backref("role", lazy=True))
 
 
 class User(UserMixin, db.Model):
@@ -20,8 +22,9 @@ class User(UserMixin, db.Model):
     contact = db.Column(db.String(12), nullable=False, unique=True)
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(200), primary_key=False, unique=False, nullable=False)
-    created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True)
-    last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
+    
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
+    
 
     def set_password(self, password):
         """Create hashed password."""
@@ -37,4 +40,4 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} {}>'.format(self.fname, self.lname)
