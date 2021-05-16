31d4b15f1e5dbe4ed0a09c800cc70cd026f577e6
Harsh Mehta <devharsh3.0@gmail.com>
2021-05-16 01:22:47 +0100

    Initialised Celery, Flask-Mail and Flask-Session as well as converted relative imports to absolute imports.

--- application/__init__.py
+++ application/__init__.py
@@ -1,27 +1,35 @@
 """Initialize app."""
 
 from flask import Flask
+from celery import Celery
+from flask_config import FlaskConfig
+import celery_config
+from application.celery_utils import init_celery
+from application.extensions import db, login_manager, migrate, session, mail
+
+
+# Instantiating Celery app
+celery = Celery(__name__, broker=FlaskConfig.CELERY_BROKER_URL)
 
 
 def init_app():
     """Construct the core app object."""
     app = Flask(__name__, template_folder="templates", static_folder="static")
+    app.config.from_object(FlaskConfig)
 
-    # Application Configuration
-    app.config.from_object('config.Config')
-
-    # Initialize Plugins
-    from .extensions import db, login_manager, migrate
-    
+    """Initialize Plugins."""
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+    session.init_app(app)
+    mail.init_app(app)
 
+    init_celery(celery, app)
 
     with app.app_context():
-        from . import routes
-        from . import auth
-        
+        from application import routes
+        from application import auth
+    
         # Register Blueprints
         app.register_blueprint(auth.auth_bp)
 
