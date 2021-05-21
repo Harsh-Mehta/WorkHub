e2851faf245b57be51c54cbef972775bbee99336
Viktorija S <viktorijax4@gmail.com>
2021-05-21 22:32:06 +0100

    Resolving merging issues

--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,19 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, session, request
 from flask import current_app as app
 from flask_login import logout_user, login_required
 from application.tasks import send_email
+from application.models import Job, User, AppliedJob, JobStatus, JobSeeker
+from application.forms import JobApplicationForm
+from application.extensions import db
+
 
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 @login_required
 def home():
-    return render_template("pages/home.jinja2")
+    jobs = Job.query.all()
+    return render_template("pages/home.jinja2", jobs=jobs, session=session)
 
 
 @app.route("/sendemail", methods=["GET", "POST"])
@@ -26,12 +31,36 @@ def email_page():
 
     # return redirect(url_for('index'))
 
+
+@app.route("/apply_job/<job_id>", methods=["GET", "POST"])
+@login_required
+def apply_job(job_id):
+    form = JobApplicationForm()
+
+    if not form.validate_on_submit() and request.method != 'POST':
+        user = User.query.filter_by(id=session.get("_user_id")).first()
+        return render_template("pages/job_apply.jinja2", current_user=user, job_id=job_id)
+
+    job_exists = AppliedJob.query.filter_by(job_id=job_id).first()
+    open_status = JobStatus.query.filter_by(name="Open").first()
+    waiting_status = JobStatus.query.filter_by(name="Waiting").first()
+    job_seeker = JobSeeker.query.filter_by(user_id=session.get("_user_id")).first()
+
+    if job_exists is None or job_exists.status_id != open_status.id:
+        applied_job = AppliedJob(job_id=int(job_id), seeker_id=job_seeker.id, status_id=waiting_status.id)  # Applied
+        db.session.add(applied_job)
+        db.session.commit()
+        
+        return redirect(url_for("home"))
+
+
 @app.route("/logout")
 @login_required
 def logout():
     """User log-out logic."""
     logout_user()
     return redirect(url_for('auth_bp.login'))
+
 
 @app.route("/search")
 @login_required
